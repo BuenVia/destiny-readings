f7a98b77ca82063812bc1627ce372d92b1833843
Matt Clifford <matthewclifford@hotmail.co.uk>
2023-11-26 21:49:57 +0000

    change to bg and fonts

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='bg-image'></div>
-      <div className='bg-text'>
-        <h1>Destiny Readings</h1>
-        <p>Coming Soon...</p>
+      {/* Top Section */}
+      <div className='section'>
+        <div className='bg-text'>
+          <h1>Destiny Reading's</h1>
+          <p>Tarot, Oracle Readings</p>
+          <p>Afternoon Tarot Tea Parties</p>
+          <h1>DR</h1>
+          <p>CONTACT US</p>
+        </div>
       </div>
+
+      {/* Bottom Section */}
+      <div className='section'>
+        <div className='bg-text'>
+          <p>TEL: 07946 806655</p>
+          <p>EMAIL: hello@destinyreadings.co.uk</p>
+          <h1>DR</h1>
+          <p>"Bring me your concerns and together we will find a way forward"</p>
+        </div>
+      </div>
+      
     </div>
   );
 }
