4f66728da217f6e6efdf428f235f75ab697b2afe
Matt Clifford <matthewclifford@hotmail.co.uk>
2023-11-26 22:09:55 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -9,21 +9,27 @@ function App() {
           <h1>Destiny Reading's</h1>
           <p>Tarot, Oracle Readings</p>
           <p>Afternoon Tarot Tea Parties</p>
-          <h1>DR</h1>
-          <p>CONTACT US</p>
+          <div className='dr__ltr'>
+            <span className='d__ltr'>D</span>
+            <span className='r__ltr'>R</span>
+          </div>
+          <a href='#contact'><button>CONTACT US</button></a>
         </div>
       </div>
 
       {/* Bottom Section */}
-      <div className='section'>
+      <div className='section' id='contact'>
         <div className='bg-text'>
-          <p>TEL: 07946 806655</p>
-          <p>EMAIL: hello@destinyreadings.co.uk</p>
-          <h1>DR</h1>
+          <p><span className='bold'>TEL:</span> 07946 806655</p>
+          <p><span className='bold'>EMAIL:</span> hello@destinyreadings.co.uk</p>
+          <div className='dr__ltr'>
+            <span className='d__ltr'>D</span>
+            <span className='r__ltr'>R</span>
+          </div>
           <p>"Bring me your concerns and together we will find a way forward"</p>
         </div>
       </div>
-      
+
     </div>
   );
 }
